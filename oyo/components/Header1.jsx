dbe808452b00b19466ec962ef4a922b55eb764a4
Krish Poptani <poptanikrish1@gmail.com>
2024-04-14 15:34:44 +0530

    Home page with nav bar and banners

---
+++ oyo/components/Header1.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Image from "next/image";
+import Block from "@/components/Block";
+const Header1 = () => {
+  return (
+    <div className="flex justify-between border-b-2 border-gray-300 items-center h-24 px-10">
+      <Image
+        src={"/logo (1).png"}
+        alt="logo"
+        width={200}
+        height={200}
+        className="w-24 h-24"
+      />
+      <div className="border-r-2 border-gray-300 h-full flex ">
+        <Block
+          title={"Become a member"}
+          para={"Additional 10% off on stays."}
+        />
+        <Block title={"OYO for business"} para={"Trusted By 5000 corporates"} />
+        <Block
+          title={"List your property"}
+          para={"Start earning in 30 mins."}
+        />
+        <Block title={"987654321"} para={"Call us to book now."} />
+        <div className="flex items-center px-3">
+          <Image
+            src={"/logo (1).png"}
+            alt="demo"
+            width={200}
+            height={200}
+            className="w-10 h-10 rounded-full mr-5"
+          />
+          <h3 className="font-bold">Login/Signup</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header1;
